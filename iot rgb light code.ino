22e4057a772827841c1f7cbaf6b60be327d545b9
ziauddin <59949061+jiyaurali02@users.noreply.github.com>
2022-11-29 17:17:41 +0530

    Update iot rgb light code.ino

--- iot rgb light code.ino
+++ iot rgb light code.ino
@@ -7,35 +7,23 @@ void setup() {
   pinMode(lb, OUTPUT);
 }
 void loop() {
-  //analogWrite(lr,255);  // Red
-  //analogWrite(lg,0);
-  //analogWrite(lb,0);
-  //delay(1000);
-  
-  //analogWrite(lg,255); // Green
-  //analogWrite(lr,0);
-  //analogWrite(lb,0);
-  //delay(1000);
-  
-  //analogWrite(lb,255); // Blue
-  //analogWrite(lg,0);
-  //analogWrite(lr,0);
-  //delay(1000);
-  
-  //analogWrite(lb,255); // yellow
-  //analogWrite(lg,255);
-  //analogWrite(lr,255);
-  //delay(1000);
-  
-  //analogWrite(lb,150); // Blue
-  //analogWrite(lg,10);
-  //analogWrite(lr,10);
-  //delay(1000);
-  
-  analogWrite(lb,200); // Blue
-  analogWrite(lg,100);
-  analogWrite(lr,100);
+  analogWrite(lr,255);  // Red
+  analogWrite(lg,0);
+  analogWrite(lb,0);
   delay(1000);
   
+  analogWrite(lg,255); // Green
+  analogWrite(lr,0);
+  analogWrite(lb,0);
+  delay(1000);
   
+  analogWrite(lb,255); // Blue
+  analogWrite(lg,0);
+  analogWrite(lr,0);
+  delay(1000);
+  
+  analogWrite(lb,255); // yellow
+  analogWrite(lg,255);
+  analogWrite(lr,255);
+  delay(1000);
 }
